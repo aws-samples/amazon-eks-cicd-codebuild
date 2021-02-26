c58e6a504956e580a3bb922e7dbb30ed5a51fb81
Pahud Hsieh <pahudnet@gmail.com>
2021-02-26 19:49:53 +0800

    add projen-automate-it support (#37)
    * add projen-automate-it support
    
    * minor fix

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
+const { Automation } = require('projen-automate-it');
 
-const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.82.0',
@@ -15,48 +16,16 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-events-targets',
   ],
   dependabot: false,
-  defaultReleaseBranch: ['master'],
+  deps: ['projen-automate-it'],
+  defaultReleaseBranch: 'master',
 });
 
-// create a custom projen and yarn upgrade workflow
-const workflow = project.github.addWorkflow('ProjenYarnUpgrade');
+const automation = new Automation(project, { automationToken: AUTOMATION_TOKEN });
+automation.autoApprove();
+automation.autoMerge();
+automation.projenYarnUpgrade();
+automation.projenYarnUpgrade('projenYarnUpgradeWithTest', { yarnTest: true });
 
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'dockerAssets.d', 'yarn-error.log'];
 project.npmignore.exclude('images', ...common_exclude);

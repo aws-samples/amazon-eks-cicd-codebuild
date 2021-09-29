30f15f68147282fd7fe1fbe6d7d1f2931f066b4d
Pahud Hsieh <pahudnet@gmail.com>
2021-09-29 16:44:55 +0000

    support gitpod

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
+const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism, DevEnvironmentDockerImage, Gitpod } = require('projen');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
@@ -14,7 +14,6 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-codecommit',
     '@aws-cdk/aws-events-targets',
   ],
-  minNodeVersion: '14.17.0',
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
     ignoreProjen: false,
     workflowOptions: {
@@ -30,8 +29,42 @@ const project = new AwsCdkTypeScriptApp({
 });
 
 project.package.addField('resolutions', {
-  'pac-resolver': '5.0.0',
+  'pac-resolver': '^5.0.0',
+  'set-value': '^4.0.1',
+  'ansi-regex': '^5.0.1',
 });
+
+
+const gitpodPrebuild = project.addTask('gitpod:prebuild', {
+  description: 'Prebuild setup for Gitpod',
+});
+// install and compile only, do not test or package.
+gitpodPrebuild.exec('yarn install --frozen-lockfile --check-files');
+gitpodPrebuild.exec('npx projen compile');
+
+let gitpod = new Gitpod(project, {
+  dockerImage: DevEnvironmentDockerImage.fromImage('public.ecr.aws/pahudnet/gitpod-workspace:latest'),
+  prebuilds: {
+    addCheck: true,
+    addBadge: true,
+    addLabel: true,
+    branches: true,
+    pullRequests: true,
+    pullRequestsFromForks: true,
+  },
+});
+
+gitpod.addCustomTask({
+  init: 'yarn gitpod:prebuild',
+  // always upgrade after init
+  command: 'npx projen upgrade',
+});
+
+gitpod.addVscodeExtensions(
+  'dbaeumer.vscode-eslint',
+  'ms-azuretools.vscode-docker',
+  'AmazonWebServices.aws-toolkit-vscode',
+);
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'dockerAssets.d', 'yarn-error.log'];
 project.npmignore.exclude('images', ...common_exclude);

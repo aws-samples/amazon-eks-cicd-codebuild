a6ad33a23d7aac955196839425fd2da7bd42638f
Pahud Hsieh <pahudnet@gmail.com>
2021-12-06 09:08:21 +0000

    bump projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,8 @@
-const { AwsCdkTypeScriptApp, DevEnvironmentDockerImage, Gitpod } = require('projen');
+const { awscdk, DevEnvironmentDockerImage, Gitpod } = require('projen');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
-const project = new AwsCdkTypeScriptApp({
+const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '1.82.0',
   name: 'eks-cicd-codebuild',
   cdkDependencies: [
